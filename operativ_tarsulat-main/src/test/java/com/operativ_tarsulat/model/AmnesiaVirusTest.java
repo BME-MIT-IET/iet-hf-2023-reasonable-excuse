10506fa07ceb532e84aed4510f69d36bad893d11
kovacsnoi <66200516+kovacsnoi@users.noreply.github.com>
2023-05-12 20:39:14 +0200

    #2 PomWorking

---
+++ operativ_tarsulat-main/src/test/java/com/operativ_tarsulat/model/AmnesiaVirusTest.java
@@ -0,0 +1,19 @@
+package com.operativ_tarsulat.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AmnesiaVirusTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+    @Test
+    void checkIfVirologistForget() {
+
+
+    }
+}
