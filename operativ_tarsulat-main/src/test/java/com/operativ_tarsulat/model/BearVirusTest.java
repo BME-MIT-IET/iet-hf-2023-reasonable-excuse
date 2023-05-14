e5fc05927d4b2f05d937d683dc5fdbfede81724f
kovacsnoi <66200516+kovacsnoi@users.noreply.github.com>
2023-05-14 12:27:31 +0200

    #2 Tests rename, fix

--- operativ_tarsulat-main/src/test/java/com/operativ_tarsulat/model/BearVirusTest.java
+++ operativ_tarsulat-main/src/test/java/com/operativ_tarsulat/model/BearVirusTest.java
@@ -3,6 +3,7 @@ package com.operativ_tarsulat.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,22 +12,29 @@ class BearVirusTest {
 
     public Virologist v;
     public Virologist v2;
-    public Field f1;
-    public Laboratory f2;
+    public Laboratory f;
     public DanceGeneticCode dgc;
+    public boolean containsBearVirus;
 
     @BeforeEach
     void setUp() {
-        f1 = new FreeField();
         v2 = new Virologist();
         dgc = new DanceGeneticCode();
-        f2 = new Laboratory(dgc, true);
-        v = new Virologist("name", f2);
+        f = new Laboratory(dgc, true);
+        v = new Virologist();
+        v.SetField(f);
+        containsBearVirus = false;
     }
 
     @Test
-    void checkIfVirologistGetBearVirus() {
+    void testInteractWithField_FieldContainBearVirus_shouldActiveAgentContainBearVirus() {
         v.InteractWithField();
-        assertNotEquals(0, v.getActiveAgent().size());
+        for(Agent a : v.getActiveAgent()){
+            if(a instanceof BearVirus){
+                containsBearVirus = true;
+                break;
+            }
+        }
+        assertTrue(containsBearVirus);
     }
 }
--- operativ_tarsulat-main/src/test/java/com/operativ_tarsulat/model/ParalyzeVirusTest.java
+++ operativ_tarsulat-main/src/test/java/com/operativ_tarsulat/model/ParalyzeVirusTest.java
@@ -3,6 +3,8 @@ package com.operativ_tarsulat.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParalyzeVirusTest {
@@ -13,35 +15,41 @@ class ParalyzeVirusTest {
     public Field f1;
     public Field f2;
     public DanceGeneticCode dgc;
+    public Axe axe;
 
     @BeforeEach
     void setUp() {
-        v2 = new Virologist();
-        pv = new ParalyzeVirus(2, v2);
         dgc = new DanceGeneticCode();
         f1 = new FreeField();
         f2 = new FreeField();
-        v = new Virologist("name", f1);
+        v2 = new Virologist();
+        pv = new ParalyzeVirus(2, v2);
+        v = new Virologist();
+        axe = new Axe();
+        v.SetField(f1);
+        v2.SetField(f2);
     }
 
     @Test
-    void checkIfVirologistCanMove() {
+    void testMove_WithParalyzeVirus_shouldNotMove() {
         v.ReceiveAgentUse(pv, v2);
         v.Move(f2);
         assertNotEquals(f2, v.GetField());
     }
 
     @Test
-    void checkIfVirologistCanCreateAgent() {
+    void testCreateAgent_WithParalyzeVirus_shouldNotAgentInventoryContainCreatedAgent() {
         v.ReceiveAgentUse(pv, v2);
         v.CreateAgent(dgc);
-        assertEquals(0, v.getAgentInventory().size());
+        assertFalse(v.getAgentInventory().contains(dgc));
     }
+
     @Test
-    void checkIfVirologistCanAttack() {
+    void testAttack_WithParalyzeVirus_shouldNotRemoveAttackedVirologist() {
         v.ReceiveAgentUse(pv, v2);
         v.Attack(v2);
-        assertNotNull(v2);
+        assertFalse(Arrays.asList(f2.GetVirologists()).contains(v2));
     }
+
 
 }
